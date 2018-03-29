8e40135749577090b5c2e8f6aeda24a494edd18e
bantyK <bantyp92@gmail.com>
2018-03-29 13:42:50 +0530

    tutorial 1 comple

--- android/tutorials/tutorial1/src/main/java/com/uber/rib/root/logged_out/LoggedOutInteractor.java
+++ android/tutorials/tutorial1/src/main/java/com/uber/rib/root/logged_out/LoggedOutInteractor.java
@@ -1,6 +1,8 @@
 package com.uber.rib.root.logged_out;
 
+import android.annotation.SuppressLint;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.uber.rib.core.Bundle;
 import com.uber.rib.core.Interactor;
@@ -10,34 +12,50 @@ import com.uber.rib.core.Router;
 
 import javax.inject.Inject;
 
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+
 /**
  * Coordinates Business Logic for {@link LoggedOutScope}.
- *
+ * <p>
  * TODO describe the logic of this scope.
  */
 @RibInteractor
 public class LoggedOutInteractor
-    extends Interactor<LoggedOutInteractor.LoggedOutPresenter, LoggedOutRouter> {
+        extends Interactor<LoggedOutInteractor.LoggedOutPresenter, LoggedOutRouter> {
 
-  @Inject LoggedOutPresenter presenter;
+    private static final String TAG = "LoggedOutInteractor";
 
-  @Override
-  protected void didBecomeActive(@Nullable Bundle savedInstanceState) {
-    super.didBecomeActive(savedInstanceState);
-
-    // TODO: Add attachment logic here (RxSubscriptions, etc.).
-  }
-
-  @Override
-  protected void willResignActive() {
-    super.willResignActive();
-
-    // TODO: Perform any required clean up here, or delete this method entirely if not needed.
-  }
+    @Inject
+    LoggedOutPresenter presenter;
 
 
-  /**
-   * Presenter interface implemented by this RIB's view.
-   */
-  interface LoggedOutPresenter { }
+    @Override
+    protected void didBecomeActive(@Nullable Bundle savedInstanceState) {
+        super.didBecomeActive(savedInstanceState);
+
+        Disposable subscribe = presenter
+                .loginName()
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String name) throws Exception {
+                        Log.d(TAG, name);
+                    }
+                });
+    }
+
+    @Override
+    protected void willResignActive() {
+        super.willResignActive();
+
+    }
+
+
+    /**
+     * Presenter interface implemented by this RIB's view.
+     */
+    interface LoggedOutPresenter {
+        Observable<String> loginName();
+    }
 }
--- android/tutorials/tutorial1/src/main/java/com/uber/rib/root/logged_out/LoggedOutView.java
+++ android/tutorials/tutorial1/src/main/java/com/uber/rib/root/logged_out/LoggedOutView.java
@@ -1,25 +1,61 @@
 package com.uber.rib.root.logged_out;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.uber.rib.tutorial1.R;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 
 /**
  * Top level view for {@link LoggedOutBuilder.LoggedOutScope}.
  */
-class LoggedOutView extends View implements LoggedOutInteractor.LoggedOutPresenter {
+class LoggedOutView extends LinearLayout implements LoggedOutInteractor.LoggedOutPresenter {
 
-  public LoggedOutView(Context context) {
-    this(context, null);
-  }
+    private static final String TAG = "LoggedOutView";
+    private TextView name;
+    private Button loginButton;
 
-  public LoggedOutView(Context context, @Nullable AttributeSet attrs) {
-    this(context, attrs, 0);
-  }
+    public LoggedOutView(Context context) {
+        this(context, null);
+    }
 
-  public LoggedOutView(Context context, @Nullable AttributeSet attrs, int defStyle) {
-    super(context, attrs, defStyle);
-  }
+    public LoggedOutView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public LoggedOutView(Context context, @Nullable AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        Log.d(TAG, "onFinishInflate: ");
+        super.onFinishInflate();
+        name = (TextView) findViewById(R.id.edit_text);
+        loginButton = (Button) findViewById(R.id.login_button);
+    }
+
+    @Override
+    public Observable<String> loginName() {
+        return Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(final ObservableEmitter<String> emitter) {
+                LoggedOutView.this.loginButton.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        emitter.onNext(name.getText().toString());
+                    }
+                });
+            }
+        });
+    }
 }
